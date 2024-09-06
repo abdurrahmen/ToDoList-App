4a5888e9b3100400ec29beabdeef7a7a49bcf07a
abderrahmen DEKOUNE <abderrahmendekoune@gmail.com>
2024-09-06 23:51:26 +0100

    another font link edit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Footer from '@components/Footer';
 import NavBar from '@components/NavBar';
 import '@styles/globals.css';
+import Link from 'next/link';
 
 export const metadata = {
     title: "todolist",
@@ -11,13 +12,18 @@ export default function RootLayout({children}) {
 
   return (
     <html lang="en">
-        <body className='dark'>
-            <main className='flex flex-col p-4 items-center min-h-[100vh]' >
-                <NavBar />
-                {children}
-                <Footer />
-            </main>
-        </body>
+      <head >
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+    </head>
+      <body className='dark'>
+          <main className='flex flex-col p-4 items-center min-h-[100vh]' >
+              <NavBar />
+              {children}
+              <Footer />
+          </main>
+      </body>
     </html>
   )
 }
